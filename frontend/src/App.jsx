14a59b60c91e6a2acd0f2246174f5fa796637118
Mohamed Hesham Sayed <mhesham.menisy@aucegypt.edu>
2026-09-16 12:30:33 +0300

    Route root to landing page and radar to dedicated path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage.jsx'
 import RadarPage from './pages/RadarPage.jsx'
 import StockDetailPage from './pages/StockDetailPage.jsx'
 import './App.css'
@@ -7,7 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RadarPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/radar" element={<RadarPage />} />
         <Route path="/stocks/:symbol" element={<StockDetailPage />} />
       </Routes>
     </BrowserRouter>
