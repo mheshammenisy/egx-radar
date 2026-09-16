07d0d4d7c5f9fe80a439fbc52ec42a9e94e11548
Mohamed Hesham Sayed <mhesham.menisy@aucegypt.edu>
2026-09-16 12:58:13 +0300

    Load CaptoX theme styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './language.css'
+import './theme.css'
 import App from './App.jsx'
 import { LanguageProvider } from './i18n/LanguageContext.jsx'
 
